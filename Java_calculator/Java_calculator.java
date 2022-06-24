64add62663489d441086250b862470f739f70456
Sonia Alam <107405330+sonia005@users.noreply.github.com>
2022-06-24 10:29:12 +0600

    Add files via upload

---
+++ Java_calculator/Java_calculator.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+public class Java_calculator {
+
+
+    public static void main(String[] args) {
+        double num1,num2;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter First number :");
+        num1=scanner.nextDouble();
+        
+        System.out.print("Enter second number : ");
+        num2=scanner.nextDouble();
+                
+        System.out.print("Enter opertor(+,-,*,/) :");
+        char operator=scanner.next().charAt(0);
+        
+        
+        scanner.close();
+        double output;
+        
+        switch(operator)
+        {
+         
+            case '+':
+             output=num1+num2;
+                 break;
+                 
+            case '-':
+              output=num1-num2;
+              break;
+              
+            case '*':
+                output=num1*num2;
+                break;
+            case '/':
+                output=num1/num2;
+                        break;
+           default:
+                System.out.printf("Entered wrong operator");
+                return;
+        }
+        System.out.println(num1+" "+operator+" "+num2+": "+output);
+
+    }
+    
+}
+      
